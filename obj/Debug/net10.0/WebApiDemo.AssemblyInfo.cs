bf198bd60353c09d025556708c8462a14b3e1a67
Gaspar <perisgaspar09@gmail.com>
2026-02-02 14:54:15 -0300

    Se actualizo

--- obj/Debug/net10.0/WebApiDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApiDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f985c772f28eda6f79420867554ab86d7f6e159f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
